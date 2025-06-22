0b1ad7c3879127f139afa255bb374cf2229b4852
Kocoli <nicolielesbao.ch036@academico.ifsul.edu.br>
2025-06-22 19:09:56 -0300

    Reestruturando

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,18 +1,8 @@
 <?php
 
-// const CONF_DB_HOST = "mysql";
 const CONF_DB_HOST = "localhost";
-
-const CONF_DB_USER = "root";
-
-// const CONF_DB_PASS = "asdf1234";
-const CONF_DB_PASS = "";
-
-//const CONF_DB_NAME = "db-inf-3at";
 const CONF_DB_NAME = "db_otakustore";
-
-// const CONF_URL_TEST = "http://localhost:8080/mvc-project-tarde";
-const CONF_URL_TEST = "http://localhost/sua-pasta";
-
-const CONF_URL_BASE = "http://localhost:8080/mvc-project-tarde";
-// const CONF_URL_BASE = "http://localhost/sua-pasta";
+const CONF_DB_USER = "root";
+const CONF_DB_PASS = "";
+const CONF_URL_TEST = "http://localhost/otaku";
+const CONF_URL_BASE = "http://localhost/otaku";
